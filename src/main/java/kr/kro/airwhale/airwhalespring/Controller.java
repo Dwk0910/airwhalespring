ff6c05bb5abe5ba43525354e94cfc93560986cab
Dwk0910 <dongwan204@naver.com>
2024-02-10 06:26:25 +0900

    updated login method

--- src/main/java/kr/kro/airwhale/airwhalespring/Controller.java
+++ src/main/java/kr/kro/airwhale/airwhalespring/Controller.java
@@ -1,21 +1,54 @@
 package kr.kro.airwhale.airwhalespring;
 
 // Spring Import
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.Map;
 
 @RestController
 @CrossOrigin(origins = "*")
 public class Controller {
+    public static boolean checkPassword(Map<String, Object> post) {
+        return post.get("ctpd").equals("AiRwHaLeBaCkEnDsPrInG");
+    }
+
     @RequestMapping("/login")
     public Object login(@RequestParam Map<String, Object> post) {
-        String currentPath = System.getProperty("user.dir");
-        File db = new File(currentPath + File.separator + "database.db");
-        return true;
+        if (!checkPassword(post)) return null;
+        try {
+            String id = post.get("id").toString();
+            String pwd = post.get("pwd").toString();
+
+            String currentPath = System.getProperty("user.dir");
+            File dbf = new File(currentPath + File.separator + "database.db");
+
+            if (!dbf.exists()) {
+                if (!dbf.createNewFile()) throw new Exception();
+                FileWriter writer = new FileWriter(dbf);
+                writer.write("{}");
+                writer.flush();
+                writer.close();
+            }
+
+            JSONParser parser = new JSONParser();
+            FileReader reader = new FileReader(dbf);
+
+            JSONObject db = (JSONObject) parser.parse(reader);
+            JSONObject user = (JSONObject) db.get(id);
+
+            return user.get("password").equals(pwd);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 }
